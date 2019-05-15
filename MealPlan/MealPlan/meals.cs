975f9d21ee2542aa1a3a4461cb45b98fe6d3c728
Michelle <abg007874@hotmail.com>
2019-05-16 11:44:23 +1200

    IngredientsAndMealsClass

--- MealPlan/MealPlan/meals.cs
+++ MealPlan/MealPlan/meals.cs
@@ -9,6 +9,16 @@ namespace MealPlan
         public int ID { get; set; }
         public string Name { get; set; }
 
+        public int Ingredients_1 { get; set; }
+        public int Ingredients_2 { get; set; }
+        public int Ingredients_3 { get; set; }
+        public int Ingredients_4 { get; set; }
+        public int Ingredients_5 { get; set; }
+        public int Ingredients_6 { get; set; }
+        public int Ingredients_7 { get; set; }
+        public int Ingredients_8 { get; set; }
+        public int Ingredients_9 { get; set; }
+        public int Ingredients_10 { get; set; }
         public override string ToString()
         {
             return Name;
