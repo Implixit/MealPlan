b14cc92feaafa1e5057cf5c11a2632df13ad2e93
Michelle <abg007874@hotmail.com>
2019-05-16 09:34:09 +1200

    List with hardCode

--- MealPlan/MealPlan/MainPage.xaml.cs
+++ MealPlan/MealPlan/MainPage.xaml.cs
@@ -9,9 +9,33 @@ namespace MealPlan
 {
     public partial class MainPage : ContentPage
     {
+        //Meal List
+        public IList<meals> meals { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            meals = new List<meals>();
+            meals.Add(new meals
+            {
+                ID = 1,
+                Name = "Rice"
+            });
+
+            meals.Add(new meals
+            {
+                ID = 2,
+                Name = "Fish"
+            });
+            BindingContext = this;
+        }
+        public void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            meals selectedItem = e.SelectedItem as meals;
+        }
+        public void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            meals tappedItem = e.Item as meals;
         }
     }
 }
