03c6d7fdcacf77afc12d4c080810eb565c57d895
Charlie <charliehamilton798@gmail.com>
2019-05-28 10:16:23 +1200

    Added Home Page View

---
+++ MealPlan/MealPlan/Views/HomePage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MealPlan.Views
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            this.Title = "Select an option"; //Title page
+            StackLayout stacklayout = new StackLayout();
+
+            Button BtnAddMeal = new Button();
+            BtnAddMeal.Text = "Add a New Meal";
+            BtnAddMeal.Clicked += BtnAddMeal_Clicked;
+            stacklayout.Children.Add(BtnAddMeal);
+
+            Button BtnListMeals = new Button();
+            BtnListMeals.Text = "Get List of Meals";
+            BtnListMeals.Clicked += BtnListMeals_Clicked;
+            stacklayout.Children.Add(BtnListMeals);
+
+            Button BtnAddIngredient = new Button();
+            BtnAddIngredient.Text = "Add a New Ingredient";
+            BtnAddIngredient.Clicked += BtnAddIngredient_Clicked;
+            stacklayout.Children.Add(BtnAddIngredient);
+
+            Button BtnListIngredients = new Button();
+            BtnListIngredients.Text = "Get List of Ingredients";
+            BtnListIngredients.Clicked += BtnListIngredients_Clicked;
+            stacklayout.Children.Add(BtnListIngredients);
+
+            Content = stacklayout;
+        }
+
+        private void BtnListIngredients_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnListMeals_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnAddMeal_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnAddIngredient_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+    
